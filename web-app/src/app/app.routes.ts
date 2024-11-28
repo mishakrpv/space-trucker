fc2b00a160c658daac3f92ef97b61be04d48d9e9
mishakrpv <mishavkrpv@gmail.com>
2024-11-28 09:07:30 +0300

    feat(web): add login component

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const formatTitle = (title: string) => `${title} — Space Trucker`;
 
@@ -9,4 +10,10 @@ export const routes: Routes = [
     component: HomeComponent,
     title: formatTitle('Welcome'),
   },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: formatTitle('Log in'),
+  },
+  { path: '**', redirectTo: '' },
 ];
